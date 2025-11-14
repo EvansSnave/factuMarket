1ff0919042fceefb3d67f0d03ec68a8109a494c5
Kevin Cruz <cruzk1257@gmail.com>
2025-11-13 22:12:24 -0500

    Connect clients to audit microservice

--- clients/app/controllers/users_controller.rb
+++ clients/app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
-  # POST /client
+  # POST /clientes
   def create
     user = User.new(user_params)
 
     if user.save
+      send_audit_event(
+        type: "CLIENTE_CREADO",
+        entity_id: user.id,
+        description: "Un cliente fue creado",
+        payload: user.as_json
+      )
+
       render json: { message: "Cliente creado", user: user }, status: :created
     else
       render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
@@ -22,5 +29,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :identification)
+  end
+
+  def send_audit_event(data)
+    AuditClient.send_event(data)
   end
 end
